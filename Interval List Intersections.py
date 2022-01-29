34c94ce1fd56ebae5d67ca7c09ca2dadb2ba105f
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2022-01-28 19:32:26 -0500

    Update Interval List Intersections.py

--- Interval List Intersections.py
+++ Interval List Intersections.py
@@ -1,3 +1,11 @@
+##LEETCODE 986
+# You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj]. Each list of intervals is pairwise disjoint and in sorted order.
+# Return the intersection of these two interval lists.
+# A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+# The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+
+##THE idea of sloving the problem is define two pointer pointing two end of an intervel and by comparing the different conditions then output all overlapping parts.
+#idea is simple but should pay attention to the conditions.
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         m,n = len(firstList), len(secondList)
