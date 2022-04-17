1d008093eeec74f243d9f960ebca504f97387fec
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2022-04-17 16:48:38 -0400

    Update 386. Lexicographical Numbers.py

--- 386. Lexicographical Numbers.py
+++ 386. Lexicographical Numbers.py
@@ -14,3 +14,19 @@ def lexicalOrder(self, n: int) -> List[int]:
     for num in range(1,10):
         dfs(num)
     return ans  
+
+#this idea is also using dfs but not in a recursive way (offical solution)
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        ans = [0] * n
+        num = 1
+        for i in range(n):
+            ans[i] = num
+            if num * 10 <= n:
+                num *= 10
+            else:
+                while num % 10 == 9 or num + 1 > n:
+                    num //= 10
+                num += 1
+        return ans
+
