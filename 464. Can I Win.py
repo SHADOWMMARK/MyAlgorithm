aad00a214a8ddffdb345c2a0019f05fbcc9ef65b
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2022-05-21 18:41:18 -0400

    Create 464. Can I Win.py

---
+++ 464. Can I Win.py
@@ -0,0 +1,3 @@
+# functools.lru_cache has the ability to store time-consuming functions' results as cache, in order the funcs with same input would not be double calculate.
+
+# using dfs with memory can solve this
