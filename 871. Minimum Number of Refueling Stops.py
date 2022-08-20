c8be020dc628b26478cac90d1df985271db908cc
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2022-08-19 23:22:19 -0400

    Update 871. Minimum Number of Refueling Stops.py

--- 871. Minimum Number of Refueling Stops.py
+++ 871. Minimum Number of Refueling Stops.py
@@ -1,3 +1,4 @@
+    #this is a kind of silly way using dp (with time O(n^2))
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
         stations.sort()
         m = len(stations)
@@ -15,3 +16,20 @@
         for t, d in enumerate(dp):
             if d >= target: return t
         return -1
+# using the Priority Queue, O(NlogN) initial res = 0 and in every loop:
+# add all reachable stop to priority queue.
+# pop out the largest gas from pq and refeul once.
+# If can't refuel, means that can not go forward and return -1
+    def minRefuelStops(self, target, cur, s):
+        pq = []
+        res = i = 0
+        while cur < target:
+            while i < len(s) and s[i][0] <= cur:
+                heapq.heappush(pq, -s[i][1])
+                i += 1
+            if not pq: return -1
+            
+            cur += -heapq.heappop(pq)
+            print(cur)
+            res += 1
+        return res
