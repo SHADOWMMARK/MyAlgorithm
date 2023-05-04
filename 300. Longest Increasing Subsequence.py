dbc44f5c968413030f6854863c59b250f0e2b1f7
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2023-05-03 22:03:00 -0400

    Update 300. Longest Increasing Subsequence.py

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -1,1 +1,10 @@
 # Given an integer array nums, return the length of the longest strictly increasing
+def LIS(nums):
+    n = len(nums)
+    dp = [1]*n
+    for i in range(n):
+        for j in range(i):
+            if nums[i] > nums[j] and dp[j]+1 > dp[i]:
+                dp[i] = dp[j]+1
+    return max(dp)
+#dp: longest length end with idx i nums element
