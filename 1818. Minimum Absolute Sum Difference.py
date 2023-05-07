9501b4fa4e960641ab0708e0c2fb4e696cd3ba2b
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2023-05-06 23:10:36 -0400

    Update 1818. Minimum Absolute Sum Difference.py

--- 1818. Minimum Absolute Sum Difference.py
+++ 1818. Minimum Absolute Sum Difference.py
@@ -1,13 +1,13 @@
-obstacles = [5,1,5,5,1,3,4,5,1,4]
 import bisect
-n=len(obstacles)
-dp = [1]*n
-tempMax = 1
-for i in range(1,n):
-    x = obstacles[i]
-    idx = bisect.bisect_right(obstacles[:i-1], x)
-    print(idx,i)
-    if idx != 0:
-        dp[i] = dp[idx-1] + 1
-        
-print(dp)
+def LOCAEP(nums):
+    reco = []
+    for i,x in enumerate(nums):
+        if not reco or reco[-1]<=x:
+            reco.append(x)
+            nums[i] = len(reco)
+        else:
+            idx = bisect.bisect(reco,x)
+            reco[idx] = x
+            nums[i] = idx + 1
+    return nums
+print(LOCAEP([3,1,5,6,4,2]))
