de0db2e229ba52e8e7c99738ce96384fb269c5a6
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2023-05-08 23:18:14 -0400

    Update 981. Time Based Key-Value Store.py

--- 981. Time Based Key-Value Store.py
+++ 981. Time Based Key-Value Store.py
@@ -1,5 +1,9 @@
 import bisect
-li = [[10,'1'],[20,'2']]
-print(bisect.bisect(li,[10]))
-l = [10,15]
-print(bisect.bisect(l,10))
+# li = [[10,'1'],[20,'2']]
+# print(bisect.bisect(li,[10]))
+# l = [10,15]
+# print(bisect.bisect(l,10))
+
+intervals = [2,3,5]
+intervals.sort()
+print(bisect.bisect_left(intervals,6))
