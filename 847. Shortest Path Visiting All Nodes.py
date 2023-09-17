cef1ab3373017fb4f6d6c8911cfe847f40b75e39
SHADOWMMARK <48197733+SHADOWMMARK@users.noreply.github.com>
2023-09-17 19:08:31 -0400

    Update 847. Shortest Path Visiting All Nodes.py

--- 847. Shortest Path Visiting All Nodes.py
+++ 847. Shortest Path Visiting All Nodes.py
@@ -11,3 +11,26 @@ Output: 4
 Explanation: One possible path is [1,0,2,0,3]
 
 '''
+from collections import deque
+def shortestPathLength(graph) -> int:
+    # basic idea is using the BFS to solve but add a mask to record the route and deque = [(mask,node)..] to keep record
+    n = len(graph)
+    que = deque([(1<<i,i) for i in range(n)])
+    # and we need to record how far we have gone at this node using this mask (route)
+    dic = {(1<<i,i):0 for i in range(n)}
+
+    while que:
+        mask,node = que.popleft()
+        d = dic[(mask,node)]
+
+        if mask == (1 << n) - 1: #if every node has been visited
+            print((1 << n) - 1,1 << n - 1)
+            return d
+
+        for neighbor in graph[node]:
+            new_mask = mask | 1<<neighbor
+            if (new_mask,neighbor) not in dic.keys():
+                dic[(new_mask,neighbor)] = d+1
+                que.append((new_mask,neighbor))
+
+print(shortestPathLength(graph=[[1,2,3],[0],[0],[0]]))
